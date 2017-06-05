0814eea8d31bdb23b6520564c2833487d5861445
Karol Drwiła <k.drwila@gmail.com>
2017-06-06 01:12:51 +0200

    [BACK] Categories DAO

--- back/app/daos/ProductsDAO.scala
+++ back/app/daos/ProductsDAO.scala
@@ -9,47 +9,49 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
-  * Created by kprzystalski on 23/04/17.
-  */
+	* Created by kprzystalski on 23/04/17.
+	*/
 
 class ProductsDAO @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
-  extends HasDatabaseConfigProvider[JdbcProfile] {
+	extends HasDatabaseConfigProvider[JdbcProfile] {
 
-  import driver.api._
+	import driver.api._
 
-  val Products = TableQuery[ProductsTable]
+	val Products = TableQuery[ProductsTable]
 
-  def all(implicit ec: ExecutionContext): Future[List[ProductsREST]] = {
-    val query =  Products
-    val results = query.result
-    val futureProducts = db.run(results)
-    futureProducts.map(
-      _.map {
-        a => ProductsREST(opis = a.opis, tytul = a.tytul)
-      }.toList)
-  }
+	def all(implicit ec: ExecutionContext): Future[List[ProductsREST]] = 
+	{
+		val query =  Products
+		val results = query.result
+		val futureProducts = db.run(results)
+		futureProducts.map(
+			_.map {
+				a => ProductsREST(opis = a.opis, tytul = a.tytul)
+			}.toList)
+	}
 
-  def get(id: Long): Future[List[ProductsREST]] =
-  {
-    val query = Products.filter(_.prodId === id)
-    val results = query.result
-    val futureProducts = db.run(results)
-    futureProducts.map(
-      _.map
-      {
-        a => ProductsREST(opis = a.opis, tytul = a.tytul)
-      }.toList
-    )
-  }
+	def get(id: Long): Future[List[ProductsREST]] =
+	{
+		val query = Products.filter(_.prodId === id)
+		val results = query.result
+		val futureProducts = db.run(results)
+		futureProducts.map(
+			_.map
+			{
+				a => ProductsREST(opis = a.opis, tytul = a.tytul)
+			}.toList
+		)
+	}
 
-  def insert(product: Products): Future[Unit] = db.run(Products += product).map { _ => () }
+	def insert(product: Products): Future[Unit] = db.run(Products += product).map { _ => () }
 
 
-  class ProductsTable(tag: Tag) extends Table[Products](tag, "Products") {
-    def prodId = column[Long]("prodId",O.AutoInc, O.AutoInc)
-    def tytul = column[String]("tytul")
-    def opis = column[String]("opis")
-    def * = (prodId, tytul, opis) <> (models.Products.tupled, models.Products.unapply)
-  }
+	class ProductsTable(tag: Tag) extends Table[Products](tag, "Products") 
+	{
+		def prodId = column[Long]("prodId",O.AutoInc, O.AutoInc)
+		def tytul = column[String]("tytul")
+		def opis = column[String]("opis")
+		def * = (prodId, tytul, opis) <> (models.Products.tupled, models.Products.unapply)
+	}
 
 }
--- back/app/models/api.scala
+++ back/app/models/api.scala
@@ -3,10 +3,17 @@ package models
 import play.api.libs.json.Json
 
 /**
-  * Created by kprzystalski on 23/04/17.
-  */
+	* Created by kprzystalski on 23/04/17.
+	*/
 case class ProductsREST(tytul: String, opis: String)
+case class CategoriesREST(tytul: String, opis: String)
 
-object ProductsREST {
-  implicit val productsFormat = Json.format[ProductsREST]
+object ProductsREST 
+{
+	implicit val productsFormat = Json.format[ProductsREST]
+}
+
+object CategoriesREST 
+{
+	implicit val productsFormat = Json.format[CategoriesREST]
 }
