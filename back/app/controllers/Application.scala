8869bdef22a467b98718605926b1b2a0b460134b
Karol Drwiła <k.drwila@gmail.com>
2017-06-06 01:25:33 +0200

    [BACK] Basic categories routes, just to test.

--- back/app/controllers/Application.scala
+++ back/app/controllers/Application.scala
@@ -3,39 +3,60 @@ package controllers
 import javax.inject.Inject
 
 import daos.ProductsDAO
+import daos.CategoriesDAO
 import models.ProductsREST
+import models.CategoriesREST
 import models.Products
+import models.Categories
 import play.api.libs.json.Json
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-class Application @Inject() (productsDAO: ProductsDAO) extends Controller
+class Application @Inject() (productsDAO: ProductsDAO, categoriesDAO: CategoriesDAO) extends Controller
 {
-//  def index = Action { implicit request =>
-//    Ok(request.body)
-//  }
+	def index = Action 
+	{ implicit request =>
+		Ok(views.html.index.render(""))
+	}
 
-  def index = Action { implicit request =>
-    Ok(views.html.index.render(""))
-  }
+	def listproducts = Action.async 
+	{ implicit request =>
+		productsDAO.all map 
+		{
+			products => Ok(Json.toJson(products))
+		}
+	}
 
-  def listproducts = Action.async { implicit  request =>
-    productsDAO.all map {
-      products => Ok(Json.toJson(products))
-    }
-  }
+	def getproduct(id: Long) = Action.async 
+	{ implicit request =>
+		productsDAO.get(id) map
+		{
+			products => Ok(Json.toJson(products))
+		}
+	}
 
-  def getproduct(id: Long) = Action.async { implicit request =>
-    productsDAO.get(id) map
-    {
-      products => Ok(Json.toJson(products))
-    }
-  }
+	def newproduct = Action 
+	{ implicit request =>
+		var json:ProductsREST = request.body.asJson.get.as[ProductsREST]
+		var product = Products(prodId = 0, tytul = json.tytul, opis = json.opis)
+		productsDAO.insert(product)
+		Ok(request.body.asJson.get)
+	}
 
-  def newproduct = Action { implicit request =>
-    var json:ProductsREST = request.body.asJson.get.as[ProductsREST]
-    var product = Products(prodId = 0, tytul = json.tytul, opis = json.opis)
-    productsDAO.insert(product)
-    Ok(request.body.asJson.get)
-  }
+	def listcategories = Action.async 
+	{ implicit request =>
+		categoriesDAO.all map
+		{
+			categories => Ok(Json.toJson(categories))
+		}
+	}
+
+	def newcategory = Action 
+	{ implicit request =>
+		var json:CategoriesREST = request.body.asJson.get.as[CategoriesREST]
+		var category = Categories(catId = 0, tytul = json.tytul, opis = json.opis)
+		categoriesDAO.insert(category)
+		Ok(request.body.asJson.get)
+	}
+		
 }
--- back/conf/evolutions/default/1.sql
+++ back/conf/evolutions/default/1.sql
@@ -1,11 +1,20 @@
 # --- !Ups
 
-CREATE TABLE Products (
- "prodId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
- "tytul" VARCHAR  NOT NULL,
- "opis" TEXT NOT NULL
+CREATE TABLE Products 
+(
+	"prodId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	"tytul" VARCHAR  NOT NULL,
+	"opis" TEXT NOT NULL
+);
+
+CREATE TABLE Categories 
+(
+	"catId" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	"tytul" VARCHAR  NOT NULL,
+	"opis" TEXT NOT NULL
 );
 
 # --- !Downs
 
 DROP TABLE Products;
+DROP TABLE Categories;
